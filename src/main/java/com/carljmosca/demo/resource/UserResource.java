b38890a777b6a9bb67213aef0f923a2252fb151d
Carl J. Mosca <carl_mosca@ao.uscourts.gov>
2019-10-27 19:07:50 -0400

    added param

--- src/main/java/com/carljmosca/demo/resource/UserResource.java
+++ src/main/java/com/carljmosca/demo/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.springframework.security.access.annotation.Secured;
 
@@ -20,9 +21,12 @@ public class UserResource {
     @Secured("ROLE_USER")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getUser() {
-        String person = "some user";
-        return String.format("Hello %s", person);
+    public String getUser(@QueryParam("name") String name) {
+
+        if (name == null || name.isBlank()) {
+            name = "someone...add a name parameter to introduce yourself";
+        }
+        return String.format("Hello %s", name);
 
     }
 }
